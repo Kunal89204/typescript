078fa6d722e8936c145464b7841a8e870cf13d41
Kunal89204 <Kunalkhandelwal108@gmail.com>
2024-04-10 02:04:08 +0530

    updated

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,26 @@
 "use strict";
-let a = "hellow";
-let nambo = 3443;
-let check;
-let num = 34;
-let surname;
-console.log(typeof (a));
-console.log(typeof (num));
+// let a: string = "hellow";
+// let nambo: number = 3443;
+// let check: boolean;
+// let num = <number>34;
+const obj = {
+    height: 3432,
+    weight: 243,
+    gender: false
+};
+const obj2 = {
+    gender: false,
+    height: 342,
+    weight: 434
+};
+const obj3 = {
+    height: 342,
+    weight: 434
+};
+const func = (n, m, l) => {
+    if (typeof l === "undefined")
+        return n * m;
+    // Add a return statement for the case when `l` is provided
+    return n * m * l;
+};
+func(234, 334); // Returns the product of n and m when l is not provided
