f4d38cdf1fd0f252952976b47074e02b4ec3759b
Kunal89204 <Kunalkhandelwal108@gmail.com>
2024-04-10 00:33:16 +0530

    commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let a = "hellow";
+let nambo = 3443;
+let check;
+let num = 34;
+let surname;
+console.log(typeof (a));
+console.log(typeof (num));
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let a: string = "hellow";
+let nambo: number = 3443;
+let check: boolean;
+let num = <number>34;
+
+let surname: string|number  ;
+
+console.log(typeof(a))
+console.log(typeof(num))
