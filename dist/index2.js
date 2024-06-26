93734d9ff9057010332e42c68e95c5fe7df48c07
kunal89204 <kunalkhandelwal108@gmail.com>
2024-06-26 19:32:54 +0530

    random

---
+++ dist/index2.js
@@ -0,0 +1,46 @@
+"use strict";
+let a = "hellow";
+let nambo = 3443;
+let check;
+let num = 34;
+let names;
+// let surname: string|number  ;
+// console.log(typeof(a))
+// console.log(typeof(num))
+// const func = ((n: number, m: number) => {
+//     console.log(n + m);
+// })(2, 3);
+// type Username = (n: number, m: number) => number;
+// const func: Username = (n,m) => {
+//     console.log(n,m);
+//     return n*m;
+// }
+const arr = [1, 2, 3, 3];
+const arr2 = [2443, 3, 35, 53, 53, 3553];
+// objects in typescript
+// type Obj = {
+//     height: number,
+//     weight: number,
+//     gender?: boolean,
+// } 
+// const obj: Obj = {
+//     height: 3432,
+//     weight: 243,
+//     gender: false
+// }
+// const obj2: Obj = {
+//     gender: false,
+//     height: 342,
+//     weight: 434
+// }
+// const obj3: Obj = {
+//     height: 342,
+//     weight: 434
+// }
+// type FuncType = (n: number, m: number, l?: number) => number;
+// const func: FuncType = (n, m, l) => {
+//     if (typeof l === "undefined") return n * m;
+//     // Add a return statement for the case when `l` is provided
+//     return n * m * l;
+// };
+// func(234, 334); // Returns the product of n and m when l is not provided
--- src/index.ts
+++ src/index.ts
@@ -1,85 +1,42 @@
-let a: string = "hellow";
-let nambo: number = 3443;
-let check: boolean;
-let num = <number>34;
+let number: number
+
+number = 24434343
+
+let newnum: number
+newnum = 42355
 
 
-let names: string;
-
-
-
-// let surname: string|number  ;
-
-// console.log(typeof(a))
-// console.log(typeof(num))
-
-// const func = ((n: number, m: number) => {
-//     console.log(n + m);
-// })(2, 3);
-
-
-
-
-
-// type Username = (n: number, m: number) => number;
-
-// const func: Username = (n,m) => {
-//     console.log(n,m);
-//     return n*m;
-// }
-
-
-const arr: [number, number, number, number] = [1,2,3,3]
-const arr2:Array<number> = [2443,3,35,53,53,3553]
-
-
-
-// arrays in typescript
-// const arr: [number, number,number ] = [23,4,34];
-
-// const arr2:Array<string> = ["yo", "bhai"]
-
-
-type obj = {
-    height: number,
-    weight: number,
-    gender?:boolean
+type myobjtype = {
+    name: string,
+    age: number
 }
 
-// objects in typescript
+interface myobj {
+    name: string,
+    age: number
+}
 
-// type Obj = {
-//     height: number,
-//     weight: number,
-//     gender?: boolean,
-// } 
+const obj: myobj = {
+    name: "kunal", 
+    age: 123
+}
 
-// const obj: Obj = {
-//     height: 3432,
-//     weight: 243,
-//     gender: false
-// }
 
-// const obj2: Obj = {
-//     gender: false,
-//     height: 342,
-//     weight: 434
-// }
+type Username = Array<string | number>
 
-// const obj3: Obj = {
-//     height: 342,
-//     weight: 434
-// }
+let myarr: Username = ['1', '2', 3255]
+
+type mytype = (a: number, b: number) => number
+
+const myfunc: mytype = (a, b) => {
+    console.log(a + b)
+    return 0;
+}
 
 
 
+console.log(myarr)
 
-// type FuncType = (n: number, m: number, l?: number) => number;
+const btn = document.getElementById("heading") as HTMLHeadingElement
 
-// const func: FuncType = (n, m, l) => {
-//     if (typeof l === "undefined") return n * m;
-//     // Add a return statement for the case when `l` is provided
-//     return n * m * l;
-// };
-
-// func(234, 334); // Returns the product of n and m when l is not provided
+btn.onclick
